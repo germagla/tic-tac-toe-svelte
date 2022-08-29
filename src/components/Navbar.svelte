9500e156a5393454e1110e01cae0343fc8a3d350
Gerard Magla <gmagladev@gmail.com>
2022-08-29 10:32:16 +0000

    Complete Navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+
+<div>
+  <div class="navbar shadow-xl bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl" href="/">Tic-Tac-Toe</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal p-0">
+        <li><a href="/tic-tac-toe">Game</a></li>
+        <!--      TODO: make about page-->
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<style></style>
