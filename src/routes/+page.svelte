3e1f1c41bc96ae4f8ede85d6afcad285ade4276f
Gerard Magla <gmagladev@gmail.com>
2022-08-29 16:25:02 +0000

    Fix pnpm run dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
 <style></style>
 <div class="hero min-h-screen bg-base-200">
-  <div class="text-center">
+  <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Welcome to SvelteKit</h1>
       <p class="py-6">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
