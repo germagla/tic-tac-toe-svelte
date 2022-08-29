efd6c350e53365b375d26ed7011f51eec173a7e8
Gerard Magla <gmagladev@gmail.com>
2022-08-29 16:45:24 +0000

    Fix pnpm run dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,2 @@
 <style></style>
-<div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Welcome to SvelteKit</h1>
-      <p class="py-6">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<!--      <button class="btn btn-primary">Get Started</button>-->
-    </div>
-  </div>
-</div>
-<!--TODO: Test Discord Webhook-->
+<h1>blablabala</h1>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,25 @@ const config = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#b45309",
+          "secondary": "#fdba74",
+          "accent": "#3b82f6",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#8b5cf6",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272"
+        }
+      }
+    ]
+  },
 
-	plugins: [require("daisyui")]
+  plugins: [require("daisyui")]
 };
 
 module.exports = config;
