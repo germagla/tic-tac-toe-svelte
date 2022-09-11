ce5c8ed5fcc0018273b201e57d74ddb98e08bbbd
Gerard Magla <gmagladev@gmail.com>
2022-09-11 13:31:00 +0000

    add finishing touches #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <style></style>
-<h1>blablabala</h1>
+<h1 class="p-8 text-6xl text-center text-primary">- t i c - t a c - t o e -</h1>
+<p class="text-center text-2xl">This is a simple tic-tac-toe game I made to practice <a class="text-primary-content" href="https://kit.svelte.dev/">SvelteKit</a></p>
+<p class="text-center text-2xl">Check out the <a class="text-primary-content" href="/game">Game</a> tab for the game and the <a href="/about" class="text-primary-content">About</a> tab for information about me</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-6xl text-center text-primary italic font-thin">//TODO: Write a page about me</h1>
